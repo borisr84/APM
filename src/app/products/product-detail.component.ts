c70ec0aa2d1b6254777b41d41c9ba06b41caac1c
Boris R <borisr84@gmail.com>
2020-09-02 13:33:35 +0300

    Navigation and rounting additional techniques

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './IProduct';
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +10,25 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += ` - ${id}`;
+    this.product = {
+      'productId' : id,
+      'productName' : 'Something',
+      'productCode' : 'GUID123213',
+      'releaseDate' : 'September 2, 2020',
+      'description' : 'Something beautiful',
+      'price' : 15.00,
+      'starRating' : 2.6,
+      'imageUrl' : 'assets/images/leaf_rake.png'
+    }
+  }
+
+  onBack() : void {
+    this.router.navigate(['/products']);
   }
 
 }
